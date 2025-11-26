2ab616320c33e47ff8d6794da21f079671267285
unknown <elyas.larfi@ucdenver.edu>
2025-11-25 20:43:58 -0700

    updated

--- myosuite/envs/myo/myobase/CenterReachOut_v0.py
+++ myosuite/envs/myo/myobase/CenterReachOut_v0.py
@@ -1,125 +1,164 @@
-""" =================================================
-# Copyright (c) Facebook, Inc. and its affiliates
-Authors  :: Vikash Kumar (vikashplus@gmail.com), Vittorio Caggiano (caggiano@gmail.com)
-================================================= """
-
 import collections
 import gym
 import numpy as np
-
+import scipy.stats
 from myosuite.envs.myo.base_v0 import BaseV0
 
 
 class ReachEnvV0(BaseV0):
-    DEFAULT_OBS_KEYS = ['hand_qpos', 'hand_qvel', 'obj_pos', 'reach_err']
+    DEFAULT_OBS_KEYS = ['time','hand_qpos', 'hand_qvel', 'obj_pos', 'reach_err']#['hand_qpos', 'hand_qvel', 
     DEFAULT_RWD_KEYS_AND_WEIGHTS = {
-        "reach": 1.0,
-        "bonus": 4.0,
-        "penalty": 50,
+    'reach': 1.0,     # primary driver
+    'vel':   0.8,     # bell‐curve shaping
+    'act': 0.5,
+    # 'penalty': 1,
     }
-
+    DELAY = 3 # Delay in timesteps
+    
     def __init__(self, model_path, obsd_model_path=None, seed=None, **kwargs):
-
-        # EzPickle.__init__(**locals()) is capturing the input dictionary of the init method of this class.
-        # In order to successfully capture all arguments we need to call gym.utils.EzPickle.__init__(**locals())
-        # at the leaf level, when we do inheritance like we do here.
-        # kwargs is needed at the top level to account for injection of __class__ keyword.
-        # Also see: https://github.com/openai/gym/pull/1497
         gym.utils.EzPickle.__init__(self, model_path, obsd_model_path, seed, **kwargs)
-
-        # This two step construction is required for pickling to work correctly. All arguments to all __init__
-        # calls must be pickle friendly. Things like sim / sim_obsd are NOT pickle friendly. Therefore we
-        # first construct the inheritance chain, which is just __init__ calls all the way down, with env_base
-        # creating the sim / sim_obsd instances. Next we run through "setup"  which relies on sim / sim_obsd
-        # created in __init__ to complete the setup.
         super().__init__(model_path=model_path, obsd_model_path=obsd_model_path, seed=seed, env_credits=self.MYO_CREDIT)
-
         self._setup(**kwargs)
 
     def _setup(self,
                obj_xyz_range=None,
-               far_th=.35,
                obs_keys: list = DEFAULT_OBS_KEYS,
                drop_th=0.50,
                qpos_noise_range=None,
                weighted_reward_keys: dict = DEFAULT_RWD_KEYS_AND_WEIGHTS,
+               delay:int = DELAY,
                **kwargs,
                ):
-        self.far_th = far_th
-        self.palm_sid = self.sim.model.site_name2id("S_grasp")
+        
+        self.prep_duration = 0.04
+        self.palm_sid = self.sim.model.site_name2id("IFtip")
+        self.hand_center = self.sim.model.site_name2id("hand_center")
+        self.start_pos = self.sim.data.site_xpos[self.palm_sid].copy()
         self.object_sid = self.sim.model.site_name2id("object_o")
         self.object_bid = self.sim.model.body_name2id("Object")
-        self.obj_xyz_range = obj_xyz_range
+        self.obj_xyz_range = obj_xyz_range 
+        
+        #self.target_pos = np.copy([-0.21968516789535328, -0.21249949997036174, 1.062])
+        
         self.drop_th = drop_th
         self.qpos_noise_range = qpos_noise_range
+        self.max_episode_time = .26
+        self.solved = False
+        self.far_th=.1
+        self.previous_reach_dist = None  # track previous step distance for progression
+        
+        self.round_robin_targets = [[ 0.0613, -0.3140, 1.062], # Right 
+        [ 0.0379, -0.2574, 1.062], # Up-right 
+        [-0.0187, -0.2340, 1.062], # Up 
+        [-0.0753, -0.2574, 1.062], # Up-left 
+        [-0.0987, -0.3140, 1.062], # Left 
+        [-0.0753, -0.3706, 1.062], # Down-left 
+        [-0.0187, -0.3940, 1.062], # Down 
+        [ 0.0379, -0.3706, 1.062], # Down-right ]
+        ]
+        
+
+                   
+        self.round_robin_index = 0
+
         super()._setup(obs_keys=obs_keys,
                        weighted_reward_keys=weighted_reward_keys,
                        **kwargs,
                        )
         keyFrame_id = 0 if self.obj_xyz_range is None else 1
-        self.init_qpos[:] = self.sim.model.key_qpos[keyFrame_id].copy() #Initial position for the hand
+        self.init_qpos[:] = self.sim.model.key_qpos[keyFrame_id].copy()
+        
+        self.delay = delay
+        self.obs_buffer = collections.deque(maxlen=delay + 1)  # Store up to delay+1 observations
 
     def get_obs_vec(self):
-        self.obs_dict['time'] = np.array([self.sim.data.time])
-        self.obs_dict['hand_qpos'] = self.sim.data.qpos[:].copy()
-        self.obs_dict['hand_qvel'] = self.sim.data.qvel[:].copy() * self.dt
-        if self.sim.model.na > 0:
-            self.obs_dict['act'] = self.sim.data.act[:].copy()
-
-        # reach error
-        obs_dict['obj_pos'] = sim.data.site_xpos[self.object_sid]
-        obs_dict['palm_pos'] = sim.data.site_xpos[self.palm_sid]
-        self.obs_dict['reach_err'] = np.array(self.obs_dict['palm_pos']) - np.array(self.obs_dict['obj_pos'])
-
-        t, obs = self.obsdict2obsvec(self.obs_dict, self.obs_keys)
-        return obs
+        return 
 
     def get_obs_dict(self, sim):
         obs_dict = {}
         obs_dict['time'] = np.array([sim.data.time])
         obs_dict['hand_qpos'] = sim.data.qpos[:].copy()
         obs_dict['hand_qvel'] = sim.data.qvel[:].copy() * self.dt
+        obs_dict['fiber_length'] = sim.data.actuator_length[:].copy()
+        obs_dict['fiber_velocity'] = sim.data.actuator_velocity[:].copy() * self.dt
         if sim.model.na > 0:
             obs_dict['act'] = sim.data.act[:].copy()
-
         # reach error
         obs_dict['obj_pos'] = sim.data.site_xpos[self.object_sid]
         obs_dict['palm_pos'] = sim.data.site_xpos[self.palm_sid]
         obs_dict['reach_err'] = np.array(obs_dict['palm_pos']) - np.array(obs_dict['obj_pos'])
         return obs_dict
+    
+    def get_reward_dict(self, obs_dict):   
+        near_th = 0.005
+                
+        reach_dist = np.linalg.norm(obs_dict['reach_err'][0][0], axis=-1)
+        
+        t = float(obs_dict['time'][0])          
+        T = self.max_episode_time
+        τ = t / T
 
-    def get_reward_dict(self, obs_dict):
-        reach_dist = np.linalg.norm(obs_dict['reach_err'], axis=-1)
-        act_mag = np.linalg.norm(self.obs_dict['act'], axis=-1) / self.sim.model.na if self.sim.model.na != 0 else 0
-        far_th = self.far_th
-        near_th = 0.05 
-        drop = reach_dist > self.drop_th
-        rwd_dict = collections.OrderedDict((
-            # Optional Keys
-            ('reach', -1. * reach_dist),
-            ('bonus', 1. * (reach_dist < 2 * near_th) + 1. * (reach_dist < near_th)),
-            ('act_reg', -1. * act_mag),
-            ('penalty', -1. * (reach_dist > far_th)),
-            # Must keys
-            ('sparse', -1. * reach_dist),
-            ('solved', reach_dist < near_th),
-            ('done', reach_dist < near_th),
-        ))
-        rwd_dict['dense'] = np.sum([wt * rwd_dict[key] for key, wt in self.rwd_keys_wt.items()], axis=0)
+        # solved/done logic
+        solved = (reach_dist < near_th)
+        done = solved or (t >= T)
+        
+        σ = 0.15 
+        μ = 0.5  
+        v_target = np.exp(-0.5 * ((τ - μ) / σ)**2)
+        v_actual = np.linalg.norm(self.sim.data.qvel[:].copy() * self.dt, axis=-1)
+        vel_profile_pen = (v_actual - v_target) ** 2
+        
+        # Actuation penalty
+        act_pen = np.linalg.norm(self.sim.data.act[:].copy(), axis=-1)
+
+        # Persistent solved state
+        if solved:
+            self.solved = True
+            
+        # Reward dictionary 
+        rwd_dict = collections.OrderedDict([
+            ('reach',   float(-10 * (reach_dist))),
+            ('vel',     float(-1 * vel_profile_pen)),
+            ('penalty',   float((-1 * (reach_dist > self.far_th)))),
+            ('act', float(-0.03 * (act_pen))),  # actuation effort penalty
+        ])
+
+        rwd_dict['sparse'] = -1.0 * reach_dist
+        rwd_dict['solved'] = float(solved)
+        rwd_dict['done'] = done
+
+        # Weighted dense reward
+        rwd_dict['dense'] = np.sum([wt * rwd_dict[key] for key, wt in self.rwd_keys_wt.items() if key in rwd_dict], axis=0)
+
         return rwd_dict
     
-
-    # generate a valid target
     def generate_target_pose(self):
-        random_index = np.random.randint(0, len(self.obj_xyz_range))
-        self.sim.model.body_pos[self.object_bid] = self.obj_xyz_range[random_index]
-        self.current_object_pos = self.obj_xyz_range[random_index]
-        self.sim.forward()
-    
-        
-    def reset(self, reset_qpos=None, reset_qvel=None):
+        # Store the current index
+        self.current_direction_index = self.round_robin_index
 
-        # randomize init arms pose
+        # Select object position
+        obj_pos = np.array(self.round_robin_targets[self.round_robin_index])
+        self.round_robin_index = (self.round_robin_index + 1) % len(self.round_robin_targets)
+
+        self.sim.model.body_pos[self.object_bid] = obj_pos
+        self.current_object_pos = obj_pos
+        self.sim.forward()
+
+    def reset(self, reset_qpos=None, reset_qvel=None):
+        self.sim.data.qvel[:] = 0
+        self.sim.data.ctrl[:] = 0
+        if self.sim.model.na > 0:
+            self.sim.data.act[:] = 0
+            
+        # Reset velocity tracking for new episode
+        self.max_vel_episode = 0.0
+        self.solved = False
+        self.previous_reach_dist = None  # reset step-by-step progression tracking
+        
+        # Reset delay buffer for new episode
+        self.obs_buffer.clear()
+           
+        self.start_pos = self.sim.data.site_xpos[self.palm_sid].copy()
         if self.qpos_noise_range is not None:
             reset_qpos_local = self.init_qpos + self.qpos_noise_range*(self.sim.model.jnt_range[:,1]-self.sim.model.jnt_range[:,0])
             reset_qpos_local[-6:] = self.init_qpos[-6:]
@@ -129,4 +168,4 @@ class ReachEnvV0(BaseV0):
         self.generate_target_pose()
         self.robot.sync_sims(self.sim, self.sim_obsd)
         obs = super().reset()
-        return obs
+        return obs 
